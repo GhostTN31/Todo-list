09689b4d8e5f5bfb0c380b16d822aead21c9e868
Roshan <roshantamilselvam@gmail.com>
2024-03-03 22:35:05 +0530

    todo1

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+export const EditTodoForm = ({ editTodo, task }) => {
+    const [value, setValue] = useState(task.task)
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        editTodo(value, task.id);
+
+        setValue("");
+    }
+
+    return (
+        <form className="TodoForm" onSubmit={handleSubmit}>
+            <input type="text" className="todo-input" value={value} placeholder="Update task" onChange={handleChange} />
+            <button type="submit" className="todo-btn">Update Task</button>
+        </form>
+    )
+}
---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+    return (
+        <div className="Todo">
+            <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : "incompleted"}`}>{task.task}</p>
+            <div>
+                <FontAwesomeIcon icon={faPenToSquare} style={{ cursor: 'pointer' }} onClick={() =>
+                    editTodo(task.id)} />
+                <FontAwesomeIcon icon={faTrash} onClick={() =>
+                    deleteTodo(task.id)} style={{ cursor: 'pointer' }} />
+            </div>
+        </div>
+    )
+}
